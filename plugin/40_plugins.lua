a7b3a705ff147a2e8012d30eb5e2fc4b700273a3
Max Trapido <maxtrapido@gmail.com>
2026-01-17 16:03:56 -0700

    Sync Upstream: 2026-01-17 16:03:56

--- plugin/40_plugins.lua
+++ plugin/40_plugins.lua
@@ -122,6 +122,10 @@ later(function()
   -- - `:h conform-options`
   -- - `:h conform-formatters`
   require('conform').setup({
+    default_format_opts = {
+      -- Allow formatting from LSP server if no dedicated formatter is available
+      lsp_format = 'fallback',
+    },
     -- Map of filetype to formatters
     -- Make sure that necessary CLI tool is available
     -- formatters_by_ft = { lua = { 'stylua' } },
